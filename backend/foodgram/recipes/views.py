2d932a97ccda7d72115c81f910bb3fa7b97c00ca
deorz <TheDamnation1044@yandex.ru>
2022-08-24 22:46:42 +0300

    Правки по PEP8

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.db import transaction
 from django.db.models import Sum
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django_filters.rest_framework import DjangoFilterBackend
 from reportlab.lib.pagesizes import A4
--- backend/foodgram/utils/functions.py
+++ backend/foodgram/utils/functions.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
 
 
 def get_object(request, object_model, pk):
